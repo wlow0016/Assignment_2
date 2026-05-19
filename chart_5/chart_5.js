88ee08d673dbe801b028dcc01255cc28b5b2b856
gr4nted <wlow633@gmail.com>
2026-05-19 16:29:44 +0800

    added css

--- chart_5/chart_5.js
+++ chart_5/chart_5.js
@@ -1,4 +1,4 @@
 var vg_5 = "chart_5/chart_5.vg.json";
-vegaEmbed("#scatterplot1", vg_5).then(function(result) {
+vegaEmbed("#scatterplot1", vg_5, {width : "container"}).then(function(result) {
 
 }).catch(console.error);
