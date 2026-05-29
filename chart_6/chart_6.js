7e51e92ed5ad811cdc40fae9e4ec17ddad801695
William <wlow0016@student.monash.edu>
2026-05-29 21:02:30 +0800

    Added data for chart_6

--- chart_6/chart_6.js
+++ chart_6/chart_6.js
@@ -1,4 +1,4 @@
 var vg_6 = "chart_6/chart_6.vg.json";
-vegaEmbed("#bubble_chart1", vg_6).then(function(result) {
+vegaEmbed("#bubble_chart1", vg_6, {actions: false}).then(function(result) {
 
 }).catch(console.error);
